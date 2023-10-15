2c2f7c17523447e772b6437b91d291215826978c
Tyuiu.CherepanovVS@outlook.com <Khel102@mail.ru>
2023-10-15 21:02:17 +0500

    Task5

--- Tyuiu.CherepanovVS.Sprint2.Task5.V12/Program.cs
+++ Tyuiu.CherepanovVS.Sprint2.Task5.V12/Program.cs
@@ -7,31 +7,48 @@ using Tyuiu.CherepanovVS.Sprint2.Task5.V12.Lib;
 
 namespace Tyuiu.CherepanovVS.Sprint2.Task5.V12
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            DataService ds = new DataService();
             Console.Title = "Спринт #2 | Выполнил: Черепанов В.С. | ПКТб-23-1";
-            Console.WriteLine("****************************************************************************");
-            Console.WriteLine("*Спринт2                                                                   *");
-            Console.WriteLine("*Тема: Оператор switch                                                     *");
-            Console.WriteLine("*Задание #5                                                                *");
-            Console.WriteLine("*Вариант #12                                                               *");
-            Console.WriteLine("*Выполнил: Черепанов Вячеслав Сергеевич| ПКТб-23-1                         *");
-            Console.WriteLine("****************************************************************************");
-            Console.WriteLine("*УСЛОВИЕ:                                                                  *");
-            Console.WriteLine("* Дата некоторого дня характеризуется тремя натуральными числами: g (год), *");
-            Console.WriteLine("* m (порядковый номер месяца) и n (число). По заданным g, n и m определить *");
-            Console.WriteLine("* дату предыдущего дня. Заданный год является високосным.                  *");
-            Console.WriteLine("****************************************************************************");
-            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                          *");
-            Console.WriteLine("****************************************************************************");
-            
-            Console.WriteLine("****************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
-            Console.WriteLine("****************************************************************************");
-            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Черепанов В.С. | ПКТб-23-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch, вычисляет       *");
+            Console.WriteLine("* требуемое значение и возвращает результат. Условие: Дата некоторого дня *");
+            Console.WriteLine("* характеризуется тремя натуральными числами: g (год), m (порядковый      *");
+            Console.WriteLine("* номер месяца) и n (число). По заданным g, n и m определить дату         *");
+            Console.WriteLine("* предыдущего дня. Заданный год является високосным.                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.Write("Year = ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Month = ");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("day = ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.FindDateOfPreviousDay(year, month, day));
+
             Console.ReadKey();
         }
     }
